1c97761d59848b378d6f034a3bc5f1fa86cd641b
Peter Szujewski <pszujewski@gmail.com>
2018-12-20 10:31:29 -0500

    First test complete

--- src/controller/BaseConverter.ts
+++ src/controller/BaseConverter.ts
@@ -1,20 +1,11 @@
-import Log from "../Util";
 import { IConversionResult } from "../models";
 
 export default class BaseConverter {
-  constructor() {
-        try {
-            Log.info("Mounting base converter");
-        } catch (err) {
-            Log.error("Error");
-        }
-  }
-
-  public convertToBaseTen(binaryNumber: string): IConversionResult {
+    public convertToBaseTen(binaryNumber: string): IConversionResult {
         return { code: -1, body: { error: "" } };
-  }
+    }
 
-  public convertToBaseTwo(decimalNumber: string): IConversionResult {
+    public convertToBaseTwo(decimalNumber: string): IConversionResult {
         return { code: -1, body: { error: "" } };
-  }
+    }
 }
--- test/BaseConverter.spec.ts
+++ test/BaseConverter.spec.ts
@@ -1,5 +1,43 @@
 import { expect } from "chai";
+import { IConversionResult, IConversionSuccessBody } from "../src/models";
+import BaseConverter from "../src/controller/BaseConverter";
+
+interface IAssertionParameters {
+    input: string;
+    expected: string;
+}
 
 describe("BaseConverter convert decimal to binary and vice versa", function () {
-    // before
+    let baseConverter: BaseConverter;
+
+    before(function () {
+        baseConverter = new BaseConverter();
+    });
+
+    it("Should convert a base 10 number to base 2", () => {
+        let expectedCode: number = 0;
+        let res: IConversionResult = { code: -1, body: { error: "" } };
+        let successRes: IConversionSuccessBody;
+        let expectedResult: string = "101";
+        let test: IAssertionParameters;
+
+        const tests: IAssertionParameters[] = [
+            { input: "5", expected: "101" },
+            { input: "12", expected: "1100" },
+            { input: "21", expected: "10101" },
+        ];
+
+        for (test of tests) {
+            try {
+                res = baseConverter.convertToBaseTwo(test.input);
+            } catch (err) {
+                res = err;
+                expect.fail("Test threw unexpected error");
+            } finally {
+                successRes = (res.body as IConversionSuccessBody);
+                expect(res.code).to.equal(expectedCode);
+                expect(successRes.result).to.equal(test.expected);
+            }
+        }
+    });
 });
